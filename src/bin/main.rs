7a6156c35e4939238529db9a8cd8ba5531206ecd
Matthieu ARNAUD <matthieu.arnaud@ze-watt.com>
2025-08-14 17:06:07 +0200

    demande de mesure DHT11

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -7,7 +7,10 @@
 )]
 
 use esp_hal::clock::CpuClock;
-use esp_hal::main;
+use esp_hal::{
+    main,
+    gpio::{Level,Input,InputConfig, Output, OutputConfig},
+};
 use esp_hal::time::{Duration, Instant};
 
 #[panic_handler]
@@ -19,16 +22,25 @@ fn panic(_: &core::panic::PanicInfo) -> ! {
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();
 
+
 #[main]
 fn main() -> ! {
     // generator version: 0.5.0
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let _peripherals = esp_hal::init(config);
+    let mut led = Output::new(_peripherals.GPIO0, Level::Low, OutputConfig::default());
+    let mut DHT11 = Output::new(_peripherals.GPIO4, Level::High, OutputConfig::default());
+    led.set_high();
 
     loop {
-        let delay_start = Instant::now();
-        while delay_start.elapsed() < Duration::from_millis(500) {}
+        DHT11.set_low();
+        let mut delay_start = Instant::now();
+        while delay_start.elapsed() < Duration::from_millis(18) {}
+        DHT11.set_high();
+        delay_start =Instant::now();
+        while delay_start.elapsed() < Duration::from_micros(48) {}
+
     }
 
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-rc.0/examples/src/bin
